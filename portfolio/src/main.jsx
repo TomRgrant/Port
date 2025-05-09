e3951ff3f707ed9e18c87e38ca1dfbf3ae373379
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-09 10:07:59 +0100

    Create Architecture

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './Styles/Global.css'
+import Home from './Containers/Home'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Home/>
   </StrictMode>,
 )
