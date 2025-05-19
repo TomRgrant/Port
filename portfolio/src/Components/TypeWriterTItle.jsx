0898d50f6ed2fb326821b42296dc2d317ee4fdf2
TomRgrant <grntthomasronnie@gmail.com>
2025-05-19 19:59:46 +0100

    Added Terminal landing page

--- portfolio/src/Components/TypeWriterTItle.jsx
+++ portfolio/src/Components/TypeWriterTItle.jsx
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Typewriter from "typewriter-effect";
-import "../Styles/Components Styles/TypeWriterTItle.css"
-import "../Styles/Global.css"
+import "../Styles/Components Styles/TypeWriterTItle.css";
+import "../Styles/Global.css";
 
 function TypeWriterTitle() {
+  const [showTitle, setShowTitle] = useState(false);
+
   return (
     <>
+    <section className="intro-wrapper">
+      <div className="initialise">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(">Initialising...<br>")
+              .pauseFor(1000)
+              .typeString(">Loading Thomas_Grant_Portfolio...")
+              .callFunction(() => {
+                setShowTitle(true); // Only after this, show the title
+              })
+              .start();
+          }}
+          options={{
+            autoStart: true,
+            loop: false,
+            delay: 50,
+            cursor: "",
+          }}
+        />
+      </div>
 
-    <section className="title-wrapper">
+      {showTitle && (
+        <div className="title-wrapper">
+          <h1>Thomas Grant</h1>
+          <p className="title-description">
+            <Typewriter
+              options={{
+                strings: ["Developer", "Programmer", "Code Wizard"],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                cursor: "|",
+                deleteSpeed: 100,
+                pauseFor: 300,
+              }}
+            />
+          </p>
+        </div>
+      )}
 
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
-            .typeString(">Intilising...<br>")
-            .pauseFor(1000)
-            .typeString(">Loading Thomas_Grant_Portfolio...")
-            .start(); // just type and stop
-        }}
-        options={{
-          autoStart: true,
-          loop: false,
-          delay: 50, // typing speed
-          cursor: "" // optional
-        }}
-      />
-
-        
+      
     </section>
+    <h1 className="test">Test</h1>
     </>
   );
 }
