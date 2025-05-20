0c827564d251ac1a30ad686c30450b2dc5365aaf
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-20 15:20:36 +0100

    Added project title and basic setup of project array for nodes

--- portfolio/src/Containers/Projects.jsx
+++ portfolio/src/Containers/Projects.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
+
 
 import "../Styles/Components Styles/Projects.css"
 
 function Projects() {
 
-
+    const projects = [
+        {Title:"", Description:"", Img:"", Tech:[], GitHub:"", Demo:""}
+    ]
 
 
     return(
@@ -12,7 +16,37 @@ function Projects() {
         <>
             <section className="projects-wrapper">
 
+            <div className="projects-title-wrapper title">
 
+                <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(">cd Thomas_grant_projects <br>")
+              .pauseFor(700)
+              .typeString(">Loading Thomas_Grant_Projects...")
+              .pauseFor(300)
+              .callFunction(() => {
+                setShowTitle(true); // Only after this, show the title
+              })
+              .start();
+          }}
+          options={{
+            autoStart: true,
+            loop: false,
+            delay: 50,
+            cursor: "",
+          }}
+        />
+
+        <h1 className="title-projects">Projects</h1>
+
+            </div>
+
+            <div className="project-grid-wrapper">
+
+
+
+            </div>
 
             </section>
         </>
