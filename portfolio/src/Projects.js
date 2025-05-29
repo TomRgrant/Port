afd79e6414d3309d40fa1edb56ccc55bc1c2a47f
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-29 10:07:41 +0100

    added alt text for project imgs in projects.js

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -2,6 +2,7 @@ const projectList = [
     {
         Title: "Snorkel Trails",
         Img: "",
+        Alt:"",
         Description: `Snorkel Trails is a web app I built with another developer over two weeks as our capstone project at CodeClan. The idea was to create something useful, fun and outdoorsy - so we built a platform to help people discover snorkelling spots around Scotland.
 
                         The app lets users:
@@ -20,6 +21,7 @@ const projectList = [
     {
         Title: "Mind Blank",
         Img: "",
+        Alt:"",
         Description: `Mind Blank is a trivia quiz game created during a one-week group project with three other developers. The app allows users to choose from different categories and difficulty levels, making for a customisable and engaging quiz experience.
                         My main contribution was building the interactive timer bar - it gradually shrinks over time, giving visual feedback on how much time is left, and automatically ends the game when time runs out. I also spent a good portion of the project on debugging and polishing functionality, while other teammates focused on CSS styling and backend development.
                         The project was a great opportunity to collaborate as a team under time pressure and build a fast-paced, fun app.`,
@@ -31,6 +33,7 @@ const projectList = [
     {
         Title: "Who's That Pokémon?",
         Img: "",
+        Alt:"",
         Description: `Who's That Pokémon? is a simple but fun guessing game I built over a weekend as part of a CodeClan homework assignment. The app shows an image of a Pokémon, and the player has to guess its name by typing it in.
                         If you're stuck, you can choose to skip to a new Pokémon or reveal the name. It’s a light-hearted app focused on quick interactions and a bit of nostalgia.
                         It was a great chance to practice working with user input, simple game logic, and state management in React aswell as api functions.`,
