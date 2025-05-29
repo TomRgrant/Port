a08beb0f613749031dc3b55f41ba5576c4a4181c
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-29 08:54:38 +0100

    Added Description to snorkeltrails in projects.js

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -1,20 +1,29 @@
 const projectList = [
     {
-        Title:"Snorkel Trails", 
-        Img:"",
-        Description:"",
-        Tech:["js","react","java","html","css"],
-        Github:"https://github.com/TomRgrant/SnorkelTrails",
-        Demo:""
+        Title: "Snorkel Trails",
+        Img: "",
+        Description: `Snorkel Trails is a web app I built with another developer over two weeks as our capstone project at CodeClan. The idea was to create something useful, fun and outdoorsy - so we built a platform to help people discover snorkelling spots around Scotland.
+
+                        The app lets users:
+                        - Search for snorkel locations and filter by beach, amenities, skill level, and accessibility.
+                        - Check the weather and tide conditions at each spot.
+                        - Use an interactive map to explore locations visually.
+                        - See what kinds of sea life live in each area
+
+                        I focused mainly on the frontend functionality, while my teammate handled most of the backend work and data input for the sea life per location in Java. We collaborated closely on the design and styling, working together on the CSS to make sure everything looked and felt right.
+                        We built the frontend with React, and the backend in Java. It was a great challenge balancing real-time data (like tide and weather APIs) with user-friendly filtering and map features.`,
+        Tech: ["js", "react", "java", "html", "css"],
+        Github: "https://github.com/TomRgrant/SnorkelTrails",
+        Demo: ""
     },
     {
-        Title:"", 
-        Img:"",
-        Description:"",
-        Tech:[],
-        Github:"",
-        Demo:""
+        Title: "",
+        Img: "",
+        Description: "",
+        Tech: [],
+        Github: "",
+        Demo: ""
     }
-]
+];
 
-export default projectList
+export default projectList;
