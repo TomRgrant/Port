e810ce9be65b8adf476425ae213cd5bad92c12f6
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-29 10:20:57 +0100

    Added techNodes function and re formatted code using prettier

--- portfolio/src/Components/ProjectCard.jsx
+++ portfolio/src/Components/ProjectCard.jsx
@@ -1,36 +1,38 @@
 import React from "react";
-import "../Styles/Components Styles/ProjectCard.css"
+import "../Styles/Components Styles/ProjectCard.css";
 
 function ProjectCard(project) {
+    
+  const techNodes = (project) => {
+    return project.Tech.map((tech, index) => <div key={index}>{tech}</div>);
+  };
 
+  return (
+    <>
+      <div className="project-card-wrapper">
+        <div className="project-card">
+          <div className="project-img">
+            <img src={project.Img} alt=""></img>
+          </div>
 
-
-
-
-
-    return(
-        <>
-            <div className="project-card-wrapper">
-                <div className="project-card">
-                    <div className="project-img">
-                        <img src={project.Img} alt=""></img>
-                    </div>
-
-                    <div className="project-info">
-                        <h1 className="project-title">{project.Title}</h1>
-                        <h3 className="one-liner">{project.oneLiner}</h3>
-                        <ul className="tech-stack">{techNodes}</ul>
-                        <div className="project-expanded-info">
-                            <p className="project-description">{project.Description}</p>
-                            <a className="project-button" href={project.Github}>Github</a>
-                            <a className="project-button" href={project.Demo}>Demo</a>
-                        </div>
-                    </div>
-
-                </div>
+          <div className="project-info">
+            <h1 className="project-title">{project.Title}</h1>
+            <h3 className="one-liner">{project.oneLiner}</h3>
+            <ul className="tech-stack">{techNodes}</ul>
+            <div className="project-expanded-info">
+              <p className="project-description">{project.Description}</p>
+              <a className="project-button" href={project.Github}>
+                Github
+              </a>
+              <a className="project-button" href={project.Demo}>
+                Demo
+              </a>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProjectCard
+export default ProjectCard;
