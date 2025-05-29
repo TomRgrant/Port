4aa45fb7517db42ebe2f89cad1ea86831d27bfcc
TomRgrant <111927422+TomRgrant@users.noreply.github.com>
2025-05-29 10:01:15 +0100

    created ProjectCard.js and .css and added basic jsx to ProjectCard.jsx

---
+++ portfolio/src/Components/ProjectCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "../Styles/Components Styles/ProjectCard.css"
+
+function ProjectCard(project) {
+
+
+
+
+
+
+    return(
+        <>
+            <div className="project-card-wrapper">
+                <div className="project-card">
+                    <div className="project-img">
+                        <img src={project.Img} alt=""></img>
+                    </div>
+
+                    <div className="project-info">
+                        <h1 className="project-title">{project.Title}</h1>
+                        <h3 className="one-liner">{project.oneLiner}</h3>
+                        <ul className="tech-stack">{techNodes}</ul>
+                        <div className="project-expanded-info">
+                            <p className="project-description">{project.Description}</p>
+                            <a className="project-button" href={project.Github}>Github</a>
+                            <a className="project-button" href={project.Demo}>Demo</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectCard
